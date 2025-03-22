91b768c71460bc31eda691bf9889b07d38145011
Ernest Kamau <1.kamauernest@gmail.com>
2025-03-22 18:25:16 +0300

    fetch request

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
 // Your code here
+document.addEventListener("DOMContentLoaded", () => {
+    //Render DOM elements
+    function displayName(characters){
+        const characterBar = document.getElementById("character-bar")
+        //const characterName = document.createElement("span")
+        characterBar.innerHTML = `
+        <span> ${characters.name} </span>
+        `
+        //characterBar.appendChild(characterName)
+    }
+
+
+
+    //fetch Request
+    function getCharachters(){
+        fetch('http://localhost:3000/characters')
+        .then( (response) => response.json())
+        .then( data => data.forEach(character =>displayName(character)) )
+    }
+    getCharachters()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
