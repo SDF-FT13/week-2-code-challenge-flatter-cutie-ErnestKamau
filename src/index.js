b86303c21b16734a141dcc6c034486bf77e58948
Ernest Kamau <1.kamauernest@gmail.com>
2025-03-22 18:36:42 +0300

    Displayed the names on the character bar

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 // Your code here
 document.addEventListener("DOMContentLoaded", () => {
-    //Render DOM elements
+    const characterBar = document.getElementById("character-bar")
+
+
     function displayName(characters){
-        const characterBar = document.getElementById("character-bar")
-        //const characterName = document.createElement("span")
-        characterBar.innerHTML = `
-        <span> ${characters.name} </span>
-        `
-        //characterBar.appendChild(characterName)
+        const characterName = document.createElement("span")
+        characterName.classList.add("character-bar")
+        characterName.textContent = characters.name
+
+        characterBar.appendChild(characterName)
+        
     }
 
 
 
-    //fetch Request
     function getCharachters(){
         fetch('http://localhost:3000/characters')
         .then( (response) => response.json())
